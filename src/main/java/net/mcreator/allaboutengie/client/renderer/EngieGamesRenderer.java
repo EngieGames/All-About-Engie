29b6cbfce92db4e6e8d629c39adcf222dd21d295
EngieGames <engifr.games@gmail.com>
2025-05-22 11:39:17 -0500

    Magic

--- src/main/java/net/mcreator/allaboutengie/client/renderer/EngieGamesRenderer.java
+++ src/main/java/net/mcreator/allaboutengie/client/renderer/EngieGamesRenderer.java
@@ -2,8 +2,10 @@
 package net.mcreator.allaboutengie.client.renderer;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.layers.EyesLayer;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.RenderType;
 
 import net.mcreator.allaboutengie.entity.EngieGamesEntity;
 import net.mcreator.allaboutengie.client.model.ModelEngieGames;
@@ -11,6 +13,12 @@ import net.mcreator.allaboutengie.client.model.ModelEngieGames;
 public class EngieGamesRenderer extends MobRenderer<EngieGamesEntity, ModelEngieGames<EngieGamesEntity>> {
 	public EngieGamesRenderer(EntityRendererProvider.Context context) {
 		super(context, new ModelEngieGames(context.bakeLayer(ModelEngieGames.LAYER_LOCATION)), 0.5f);
+		this.addLayer(new EyesLayer<EngieGamesEntity, ModelEngieGames<EngieGamesEntity>>(this) {
+			@Override
+			public RenderType renderType() {
+				return RenderType.eyes(new ResourceLocation("allaboutengie:textures/entities/engiegamesentity_e.png"));
+			}
+		});
 	}
 
 	@Override
--- src/main/java/net/mcreator/allaboutengie/client/renderer/InsanityRenderer.java
+++ src/main/java/net/mcreator/allaboutengie/client/renderer/InsanityRenderer.java
@@ -2,8 +2,10 @@
 package net.mcreator.allaboutengie.client.renderer;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.layers.EyesLayer;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.RenderType;
 
 import net.mcreator.allaboutengie.entity.InsanityEntity;
 import net.mcreator.allaboutengie.client.model.ModelEngieGames;
@@ -11,6 +13,12 @@ import net.mcreator.allaboutengie.client.model.ModelEngieGames;
 public class InsanityRenderer extends MobRenderer<InsanityEntity, ModelEngieGames<InsanityEntity>> {
 	public InsanityRenderer(EntityRendererProvider.Context context) {
 		super(context, new ModelEngieGames(context.bakeLayer(ModelEngieGames.LAYER_LOCATION)), 0.5f);
+		this.addLayer(new EyesLayer<InsanityEntity, ModelEngieGames<InsanityEntity>>(this) {
+			@Override
+			public RenderType renderType() {
+				return RenderType.eyes(new ResourceLocation("allaboutengie:textures/entities/insanity_e.png"));
+			}
+		});
 	}
 
 	@Override
